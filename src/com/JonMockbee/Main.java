bdeb5a8524440590d27604a63a80c9f625b347de
Jon Mockbee <mockbee1000@gmail.com>
2019-07-25 12:19:05 -0400

    two different ways writing while loop

--- src/com/JonMockbee/Main.java
+++ src/com/JonMockbee/Main.java
@@ -3,6 +3,30 @@ package com.JonMockbee;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	 int count = 1;
+             while(count!=6){
+                 System.out.println(" count value is " + count);
+                 count++;
+             }
+
+         count = 1;
+
+             while (true){
+                 if( count == 6) {
+                     break;
+
+                 }
+
+                 System.out.println(" count value is " + count);
+                 count++;
+             }
+//
+
+
+
+
+        }
     }
-}
+
+
+
